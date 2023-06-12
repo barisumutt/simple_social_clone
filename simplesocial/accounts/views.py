c98ff414989e384f1207994f2fca7e89a709b233
barisumut <barisumutbaykal@gmail.com>
2023-06-12 21:42:54 +0300

    Add form_class, success_url, template_name to SignUp view

--- simplesocial/accounts/views.py
+++ simplesocial/accounts/views.py
@@ -7,4 +7,6 @@ from . import forms
 
 # Create your views here.
 class SignUp(CreateView):
-    pass
+    form_class = forms.UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
