c4532b1b9859e7bd893c8cfd77459661bfd13c14
barisumut <barisumutbaykal@gmail.com>
2023-06-13 12:06:59 +0300

    Create groups and posts apps with templates, urls.py, forms.py

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -16,7 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
