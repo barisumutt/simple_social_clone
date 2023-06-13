e3b55f9fce23c7410f96bf527a058ac8ba31e3cc
barisumut <barisumutbaykal@gmail.com>
2023-06-13 23:04:17 +0300

    pip install django-braces to mixins and utilities

--- simplesocial/posts/views.py
+++ simplesocial/posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
